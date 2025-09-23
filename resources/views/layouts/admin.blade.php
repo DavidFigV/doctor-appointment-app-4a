7f90479e57de316ff93781b0eb2eddab66cb3869
DavidFigV <tutroyano2001@gmail.com>
2025-09-23 09:33:36 -0600

    feat(sidebar): add support for section headers in navigation links

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://kit.fontawesome.com/6f74026cec.js" crossorigin="anonymous"></script>
         <!-- Styles -->
         @livewireStyles
     </head>
